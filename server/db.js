22915749ec3f8e908e9b239f40d066eeecf549af
wahyukmr <maryu9229@gmail.com>
2023-08-29 07:27:46 +0700

    set up database connectivity

--- server/db.js
+++ server/db.js
@@ -1,28 +1,27 @@
-const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoose = require("mongoose");
+const mongoURI = "mongodb://root:NDI2LW1hcnl1OTIy@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
-    const MAX_RETRIES = 3;
-    const count = retryCount ?? 0;
-    try {
-        await mongoose.connect(mongoURI, { dbName: 'stayhealthybeta1'});
-        console.info('Connected to Mongo Successfully')
+  const MAX_RETRIES = 3;
+  const count = retryCount ?? 0;
+  try {
+    await mongoose.connect(mongoURI, { dbName: "stayhealthybeta1" });
+    console.info("Connected to Mongo Successfully");
 
-        return;
-    } catch (error) {
-        console.error(error);
+    return;
+  } catch (error) {
+    console.error(error);
 
-        const nextRetryCount = count + 1;
+    const nextRetryCount = count + 1;
 
-        if (nextRetryCount >= MAX_RETRIES) {
-            throw new Error('Unable to connect to Mongo!');
-        }
-
-        console.info(`Retrying, retry count: ${nextRetryCount}`)
-
-        return await connectToMongo(nextRetryCount);
-
+    if (nextRetryCount >= MAX_RETRIES) {
+      throw new Error("Unable to connect to Mongo!");
     }
+
+    console.info(`Retrying, retry count: ${nextRetryCount}`);
+
+    return await connectToMongo(nextRetryCount);
+  }
 };
 
 module.exports = connectToMongo;
