7d2a012645cc62df023c70680dd135089ce9a5cd
wahyukmr <maryu9229@gmail.com>
2023-08-29 14:54:44 +0700

    update database

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://root:NDI2LW1hcnl1OTIy@127.0.0.1:27017";
+const mongoURI = "mongodb://root:Mjk2MzItbWFyeXU5@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
   const MAX_RETRIES = 3;
---
+++ src/Components/Login/Login.js
@@ -0,0 +1,68 @@
+import { Link } from "react-router-dom";
+import "./Login.css";
+
+export default function Login() {
+  return (
+    <div className="container">
+      <div className="login-grid">
+        <div className="login-text">
+          <h2>Login</h2>
+        </div>
+        <div className="login-text">
+          Are you a new member?
+          <span>
+            <Link href="../Sign_Up/Sign_Up.html" style={{ color: "#2190ff" }}>
+              Sign Up Here
+            </Link>
+          </span>
+        </div>
+        <br />
+        <div className="login-form">
+          <form>
+            <div className="form-group">
+              <label for="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="form-control"
+                placeholder="Enter your email"
+                aria-describedby="helpId"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label for="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                className="form-control"
+                placeholder="Enter your password"
+                aria-describedby="helpId"
+                required
+              />
+            </div>
+
+            <div className="btn-group">
+              <button
+                type="submit"
+                className="btn btn-primary mb-2 mr-1 waves-effect waves-light"
+              >
+                Login
+              </button>
+              <button
+                type="reset"
+                className="btn btn-danger mb-2 waves-effect waves-light"
+              >
+                Reset
+              </button>
+            </div>
+            <br />
+            <div className="login-text">Forgot Password?</div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
