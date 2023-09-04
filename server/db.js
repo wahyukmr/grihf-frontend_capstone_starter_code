ade10ad7bd12a56e77ca356948f04f38856aa49e
wahyukmr <maryu9229@gmail.com>
2023-09-04 17:21:46 -0400

    add keyword for SEO

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://root:MjczOTktbWFyeXU5@127.0.0.1:27017";
+const mongoURI = "mongodb://root:MjI1MDEtbWFyeXU5@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
   const MAX_RETRIES = 3;
